b55def6f9c67f3c991a94eebb4d3feb8bb11041f
Migle Cerniauskaite <nebaisu@gmail.com>
2026-01-17 20:25:33 +0100

    Fix another typo

--- scripts/qc-samples.sh
+++ scripts/qc-samples.sh
@@ -7,7 +7,7 @@ set -o pipefail
 PROJECT_HOME_DIR=${PROJECT_HOME_DIR:-/home/projects/22126_NGS/projects/group15}
 PROJECT_DATA_DIR=$PROJECT_HOME_DIR/data
 LOG_DIR=$PROJECT_HOME_DIR/logs
-PROEJCT_DATA_FASTQ_DIR=$PROJECT_DATA_DIR/fastqc
+PROJECT_DATA_FASTQ_DIR=$PROJECT_DATA_DIR/fastqc
 
 log_file="$LOG_DIR/$(basename $0).log"
 fastqc=/home/ctools/FastQC/fastqc
@@ -41,13 +41,13 @@ fi
 shopt -s nullglob
 for fastq_file in $input; do
     file_name="$(basename "$fastq_file")"
-    qc_report="$PROEJCT_DATA_FASTQ_DIR"/"$(echo "$file_name" | cut -d '.' -f1)_fastqc.html"
+    qc_report="$PROJECT_DATA_FASTQ_DIR"/"$(echo "$file_name" | cut -d '.' -f1)_fastqc.html"
     if [ -f "$qc_report" ]; then
         log "QC report already exists ($qc_report), skipping $fastq_file"
     else
         log "Running FastQC on $fastq_file"
         temp_output=$(mktemp)
-        $fastqc -o "$PROEJCT_DATA_FASTQ_DIR" "$fastq_file" 2>&1 | tee "$temp_output"
+        $fastqc -o "$PROJECT_DATA_FASTQ_DIR" "$fastq_file" 2>&1 | tee "$temp_output"
         log $(cat $temp_output)
         log "Done running FastQC on $fastq_file"
     fi
