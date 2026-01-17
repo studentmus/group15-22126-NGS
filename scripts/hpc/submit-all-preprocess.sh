625da26aa73a0d9adc4b62625b9f5bb26d593ff0
Migle Cerniauskaite <nebaisu@gmail.com>
2026-01-17 12:49:31 +0100

    Add helper script to submit all samples to HPC for preprocessing - take 2

--- scripts/hpc/submit-all-preprocess.sh
+++ scripts/hpc/submit-all-preprocess.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
 PROJECT_HOME_DIR=${PROJECT_HOME_DIR:-/home/projects/22126_NGS/projects/group15}
 PROJECT_DATA_DIR=$PROJECT_HOME_DIR/data
 LOG_DIR=$PROJECT_HOME_DIR/logs
 
-SRR_IDS=$(cat $PROJECT_HOME_DIR/data/samples.csv | cut -d ',' -f1)
+mkdir -p -m 775 $LOG_DIR
 
-mkdir -p $LOG_DIR
-cd $PROJECT_DATA_DIR/raw
+log_file="$LOG_DIR/$(basename $0).log"
 
-for SRR_ID in $SRR_IDS; do
-    ftp_url="ftp://$(cat $PROJECT_HOME_DIR/data/filereport_read_run_PRJNA340216.tsv | cut -f7 | grep $SRR_ID)"
-    file_name=$(basename $ftp_url)
-    if [ -f "$file_name" ]; then
-        echo "File already exists, skipping: $file_name"
+log() {
+    local msg="[$(date +"%Y-%m-%d %H:%M:%S")] [$(basename $0)] $*" 
+    echo "$msg" >> "$log_file" 2>&1
+    echo "$msg"
+}
+
+srr_ids=$(cat $PROJECT_DATA_DIR/samples.csv | cut -d ',' -f1)
+
+for srr_id in $srr_ids; do
+    fastq_file=$PROJECT_DATA_DIR/raw/$srr_id.fastq.gz
+    if [ -f $fastq_file ]; then
+        log "Submitting job for $fastq_file"
+        temp_output=$(mktemp)
+        sbatch $PROJECT_HOME_DIR/scripts/hpc/run_preprocess.sh "$fastq_file" 2>&1 | tee "$temp_output"
+        log $(cat $temp_output)
     else
-        echo "Downloading $SRR_ID from $ftp_url ($file_name)"
-        wget $ftp_url --append-output $LOG_DIR/SRR-download.log -nv
+        log "File not found: $fastq_file"
     fi
 done
-
-echo "Done"
